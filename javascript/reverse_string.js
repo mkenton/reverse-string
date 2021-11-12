f44163c5d2a7e9d56588685cf9abbafff0c17f55
Michael <mkenton2020@icloud.com>
2021-11-11 22:14:51 -0600

    reverse string - tests passed

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,27 @@
+//Reverse string fucnctino
+
 function reverseString(str) {
-  // type your code here
+
+  //convert input string into an array
+  const stringArr= Array.from(str); // alternatively use str.split('')
+
+  // calculate length of input string
+  const stringLength = stringArr.length-1
+
+  // initialize empty string for generating reverse string
+  let reversedString = ''
+ 
+  // push letters from string array into reversedString, starting from the end of string
+  for (let i=0; i <= stringLength; i++) {
+    //we can iterate in positive direction (0 up to stringLength), but still generate reversed string by pushing letters from the string array according to index = stringLength-i
+    reversedString = reversedString + stringArr[stringLength-i]
+  }
+
+  return reversedString;
 }
 
 if (require.main === module) {
-  // add your own tests in here
+
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
@@ -14,6 +32,3 @@ if (require.main === module) {
 }
 
 module.exports = reverseString;
-
-// Please add your pseudocode to this file
-// And a written explanation of your solution
